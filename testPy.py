6cf3d8340734e1963b3493ca3baf6af6125220be
Rahul Pradeep <rahulpradeep4218@gmail.com>
2019-02-16 20:59:38 -0500

    Testing GitIgnore

--- testPy.py
+++ testPy.py
@@ -8,3 +8,7 @@ print("Commit 3 and pushed")
 
 for i in range(10):
     print("i : ",i)
+
+ForexDBParameters = dict(line.strip().split('=') for line in open('forexDB.properties') if not line.startswith('#') and not line.startswith('\n'))
+print("UserName : ",ForexDBParameters['username'],"password : ",ForexDBParameters['password'])
+#print(ForexDBParameters)
